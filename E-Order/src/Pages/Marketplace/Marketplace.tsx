d7fea3d6cada0cbd686c7828c098f4e2b392722b
Test <elnur-mamedov@outlook.com>
2025-01-29 11:45:37 +0400

    Update

--- E-Order/src/Pages/Marketplace/Marketplace.tsx
+++ E-Order/src/Pages/Marketplace/Marketplace.tsx
@@ -2,13 +2,23 @@ import LanguageSwitcher from '../../Components/LanguageSwitcher/LanguageSwitcher
 import './Marketplace.scss'
 
 export default function Marketplace() {
+  
   return (
     <div className='marketplace'>
       <LanguageSwitcher />
 
-      {/* <div className='brand-search'> */}
+      <div className="search-container">
+        <i className='bx bx-search search-icon'></i>
+        <div className="search-content">
+          <h2>Nömrə üzrə axtarış</h2>
+          <input type="text" placeholder="İstehsalçı və ya orijinal kodu daxil edin" />
+          <button>Axtar</button>
+        </div>
+      </div>
 
-      {/* </div> */}
+      <div className='search-results'>
+
+      </div>
     </div>
   )
 }
