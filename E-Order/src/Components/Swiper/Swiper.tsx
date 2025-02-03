69625bfe6bd3d223f590ff9be14d7539fca4907a
Test <elnur-mamedov@outlook.com>
2025-02-03 16:12:33 +0400

    Update

--- E-Order/src/Components/Swiper/Swiper.tsx
+++ E-Order/src/Components/Swiper/Swiper.tsx
@@ -1,6 +1,6 @@
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
-
 import './Swiper.scss';
 import { EffectCoverflow, Autoplay, Pagination } from 'swiper/modules';
 
@@ -13,36 +13,45 @@ import ADS5 from '../../Assets/ADS/ADS-5.png';
 const images: string[] = [ADS1, ADS2, ADS3, ADS4, ADS5];
 
 export default function MySwiper() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className='swiper' style={{ userSelect: 'none' }}>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 100,
-          modifier: 1.5,
-        }}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}  
-        pagination={{
-          clickable: true, 
-        }}
-        modules={[EffectCoverflow, Autoplay, Pagination]}
-        className='swiper-container'
-      >
-        {images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <img src={image} alt={`slide ${index + 1}`} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div>
+      <button className='toggle-button' onClick={toggleVisibility}>Aksiyalar və elanları göstər</button>
+      <div className={`swiper ${!isVisible ? 'hidden' : ''}`} style={{ userSelect: 'none' }}>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 1.5,
+          }}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}  
+          pagination={{
+            clickable: true, 
+          }}
+          modules={[EffectCoverflow, Autoplay, Pagination]}
+          className='swiper-container'
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <img src={image} alt={`slide ${index + 1}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
-};
+}
--- E-Order/src/Pages/Marketplace/Marketplace.tsx
+++ E-Order/src/Pages/Marketplace/Marketplace.tsx
@@ -1,8 +1,7 @@
-import './Marketplace.scss'
+import './Marketplace.scss';
 import Swiper from '../../Components/Swiper/Swiper';
 
 export default function Marketplace() {
-
   return (
     <div className='marketplace'>
       <div className="buttons-container">
@@ -31,7 +30,7 @@ export default function Marketplace() {
         </button>
       </div>
 
-      <Swiper/>
+      <Swiper />
 
       <div className="search-container">
         <i className='bx bx-search search-icon'></i>
@@ -43,8 +42,68 @@ export default function Marketplace() {
       </div>
 
       <div className='search-results'>
-
+        <h2>Axtarış nəticələri</h2>
+        <input type="text" placeholder="Nəticə üzrə axtarış:" className="search-filter" />
+        <table className='results-table'>
+          <thead>
+            <tr>
+              <th>İstehsalçı (Brend)</th>
+              <th>İstehsalçı Kodu</th>
+              <th>Məhsulun Adı</th>
+              <th>Avto</th>
+              <th>Məhsulun Orijinal Kodu</th>
+              <th>Status</th>
+              <th>Miqdar</th>
+              <th>Ədəd Qiyməti (AZN)</th>
+              <th>Ədəd Qiyməti (USD)</th>
+              <th>Seç</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Brend1</td>
+              <td>001</td>
+              <td>Məhsul 1</td>
+              <td>Avto 1</td>
+              <td>AB123</td>
+              <td>Aktiv</td>
+              <td>
+                <input type="number" placeholder="Miqdar" min="0" />
+              </td>
+              <td>50 AZN</td>
+              <td>30 USD</td>
+              <td>
+                <input type="checkbox" id="select1" className="select-checkbox" />
+                <label htmlFor="select1">
+                  <i className="bx bx-check-circle"></i>
+                </label>
+              </td>
+            </tr>
+            <tr>
+              <td>Brend2</td>
+              <td>002</td>
+              <td>Məhsul 2</td>
+              <td>Avto 2</td>
+              <td>CD456</td>
+              <td>Pasif</td>
+              <td>
+                <input type="number" placeholder="Miqdar" min="0" />
+              </td>
+              <td>40 AZN</td>
+              <td>24 USD</td>
+              <td>
+                <input type="checkbox" id="select1" className="select-checkbox" />
+                <label htmlFor="select1">
+                  <i className="bx bx-check-circle"></i>
+                </label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <div className="add-to-cart">
+          <button>Əlavə et</button>
+        </div> */}
       </div>
     </div>
-  )
+  );
 }
